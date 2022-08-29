ba15553105e41f2d174560e31d0670064c2e77d0
Naminek <43778266+Naminek@users.noreply.github.com>
2022-08-29 23:26:24 +0200

    setup installation

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
+const esModules = [
+  'vuex-composition-helpers',
+  'validator/es/lib/',
+  'lodash-es',
+].join('|')
+
 module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
@@ -17,8 +23,39 @@ module.exports = {
   },
   collectCoverage: true,
   collectCoverageFrom: [
+    '<rootDir>/composables/**/*.ts',
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/store/**/*.ts',
+    '<rootDir>/models/**/*.ts'
   ],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    '<rootDir>/test/setup.ts'
+  ],
+  snapshotSerializers: [
+    '<rootDir>/node_modules/jest-serializer-vue-tjw'
+  ],
+  transformIgnorePatterns: [`node_modules/(?!${esModules})`]
 }
+
+// const esModules = [
+//   'vuex-composition-helpers',
+//   'validator/es/lib/',
+//   'lodash-es',
+// ].join('|')
+
+// module.exports = {
+//   testEnvironment: 'jsdom',
+//   moduleNameMapper: {
+//     '^~/(.*)$': '<rootDir>/$1',
+//     '^.+.(css|styl|less|sass|scss|png|jpg|webp|svg|ttf|woff|woff2)$': 'jest-transform-stub'
+//   },
+//   transformIgnorePatterns: [`node_modules/(?!${esModules})`],
+//   modulePathIgnorePatterns: ['<rootDir>/cypress/'],
+//   globals: {
+//     'ts-jest': {
+//       isolatedModules: true
+//     }
+//   }
+// }
